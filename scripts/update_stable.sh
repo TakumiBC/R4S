af2d3fb5223b04b7c0269ecb42fbca47719c9a53
Topsy Chen <62056970+thomaswcy@users.noreply.github.com>
2021-06-06 11:10:45 +0800

    Add files via upload

---
+++ scripts/update_stable.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#By: Topsy Chen (thomaswcy)
+#Licensed to the public under GPL General Public License 2.0
+echo "Welcome to the RubikWrt NanoPi R4S Update Script (Stable Firmware)"
+echo "Downloading the latest release of stable firmware"
+wget -P /tmp https://github.com/thomaswcy/R4S/releases/download/$(date +%Y.%m.%d)-stable/openwrt-rockchip-armv8-friendlyarm_nanopi-r4s-ext4-sysupgrade.img.gz
+echo "Extracting Files"
+cd /tmp/
+unzip openwrt-rockchip-armv8-friendlyarm_nanopi-r4s-ext4-sysupgrade.img.gz
+cd /
+echo "Ready to reboot for updating after 3 seconds"
+sleep 3s
+sysupgrade /tmp/openwrt-rockchip-armv8-friendlyarm_nanopi-r4s-ext4-sysupgrade.img
